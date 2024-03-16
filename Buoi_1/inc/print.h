3529b0818bbb4c6b041bdfac5f02858c67eb2d79
DucNguyen09091999 <nguyenchiduc090999@gmail.com>
2024-03-15 21:23:49 -0700

    Added first hw

---
+++ Buoi_1/inc/print.h
@@ -0,0 +1,9 @@
+#ifndef __PRINT_H__
+#define __PRINT_H__
+
+#include "data.h"
+#include "parser.h"
+
+void print_ls(file_info *files, int count, options_t *options, widths_t *widths);
+
+#endif
---
+++ Buoi_1/inc/sort.h
@@ -0,0 +1,9 @@
+#ifndef __SORT_H__
+#define __SORT_H__
+
+#include "data.h"
+#include "parser.h"
+
+void sort(file_info *files, int count, options_t *options);
+
+#endif
