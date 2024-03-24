76d652ffedba2ad9da79e55c25449fe0d311f356
DucNguyen09091999 <nguyenchiduc090999@gmail.com>
2024-03-24 00:35:21 -0700

    Update status dummy

--- Buoi_2/Bai_2/RW_Ops.c
+++ Buoi_2/Bai_2/RW_Ops.c
@@ -1,36 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+
+
+typedef int32_t status_t;
+#define E_SUCCESS   0
+#define E_FAILED    -1
 typedef struct
 {
     int fd;
 }FileHandler;
 
 
-void OpenFile(FileHandler file, const char *filename, const char *mode)
+status_t OpenFile(FileHandler file, const char *filename, const char *mode)
 {
     printf("Open file\n");
+    return E_SUCCESS;
 }
 
-void CloseFile(FileHandler file)
+status_t CloseFile(FileHandler file)
 {
     printf("Close file\n");
+    return E_SUCCESS;
 }
 
-void WriteToFile(FileHandler file, const char *content)
+status_t WriteToFile(FileHandler file, const char *content)
 {
     printf("write to file\n");
+    return E_SUCCESS;
 }
 
-void ReadFromFile(FileHandler file)
+status_t ReadFromFile(FileHandler file)
 {
     printf("write to file\n");
+    return E_SUCCESS;
 }
 
 struct OpsType {
-    void (*pOpenFile)(FileHandler file, const char *filename, const char *mode);
-    void (*pCloseFile)(FileHandler file);
-    void (*pReadFile)(FileHandler file);
-    void (*pWriteFile)(FileHandler file, const char *content);
+    status_t (*pOpenFile)(FileHandler file, const char *filename, const char *mode);
+    status_t (*pCloseFile)(FileHandler file);
+    status_t (*pReadFile)(FileHandler file);
+    status_t (*pWriteFile)(FileHandler file, const char *content);
 };
 struct CtrlType {
     const struct OpsType *const pOps;
@@ -47,15 +58,24 @@ int main(int argc, char *argv[])
 {
     FileHandler file;
     struct OpsType Rw_Ops;
+    status_t t_status;
     Rw_Ops.pOpenFile = OpenFile;
     Rw_Ops.pCloseFile = CloseFile;
     Rw_Ops.pReadFile = ReadFromFile;
     Rw_Ops.pWriteFile =  WriteToFile;
 
-    Rw_Ops.pOpenFile(file,argv[1],"w");
-    Rw_Ops.pCloseFile(file);
-    Rw_Ops.pReadFile(file);
-    Rw_Ops.pWriteFile(file,"wew");
+    t_status = Rw_Ops.pOpenFile(file,argv[1],"w");
+    if(t_status == E_SUCCESS)
+    {
+        printf("Open successfully\n");
+    }
+    else
+    {
+        printf("Open failed\n");
+    }
+    t_status = Rw_Ops.pWriteFile(file,"wew");
+    t_status = Rw_Ops.pReadFile(file);
+    t_status = Rw_Ops.pCloseFile(file);
 
     return 0;
 }
